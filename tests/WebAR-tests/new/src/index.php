bfaa3ece6a06b354324a23b0af20cdf656ee0075
Lennon Puype <lennonpuype@hotmail.com>
2020-02-01 14:00:02 +0100

    copy for webapor webapp

---
+++ tests/WebAR-tests/new/src/index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+$routes = array(
+  'home' => array(
+    'controller' => 'Immo',
+    'action' => 'index'
+  ),
+  'getuigenis' => array(
+    'controller' => 'Immo',
+    'action' => 'getuigenis'
+  )
+);
+
+if(empty($_GET['page'])) {
+  $_GET['page'] = 'home';
+}
+if(empty($routes[$_GET['page']])) {
+  header('Location: index.php');
+  exit();
+}
+
+$route = $routes[$_GET['page']];
+$controllerName = $route['controller'] . 'Controller';
+
+require_once __DIR__ . '/controller/' . $controllerName . ".php";
+
+$controllerObj = new $controllerName();
+$controllerObj->route = $route;
+$controllerObj->filter();
+$controllerObj->render();
