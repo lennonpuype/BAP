13e4a40038f33e476f81b4e75820c7d7d58fe838
Lennon Puype <lennonpuype@hotmail.com>
2020-02-10 11:41:12 +0100

    Landingpagina HTML opbouw

--- webapp/src/view/next/desktop.php
+++ webapp/src/view/next/desktop.php
@@ -1,1 +1,97 @@
-<h1>Dit is de landingpagina op desktop</h1>
+<header>
+  <nav>
+    <ul>
+      <li>NL</li>
+      <li>EN</li>
+      <li>FR</li>
+    </ul>
+  </nav>
+  <article>
+    <h1>a.r.u.next?</h1>
+    <h2>the coolest city tours worldwide</h2>
+  </article>
+  <address>
+    <ul>
+      <li>facebook</li>
+      <li>instagram</li>
+      <li>youtube</li>
+    </ul>
+  </address>
+</header>
+<main>
+  <section>
+    <figure><img src="#" alt="De installatie in het openbaar"/></figure>
+    <article>
+      <h1>a.r.u.next ? de installatie</h1>
+      <p>De a.r.u.next routes laten je op interactieve wijze kennis maken met de <b>talenten, sporen en toekomst</b> van de steden.</p>
+      <h2>Waar kan je hem vinden?</h2>
+      <p>De a.r.u.next installaties kan je vinden op centrale punten in de steden van de Eurometropool: Kortrijk, Lille, Tournai en Valenciennes. Weten waar? Check ze <span>hier</span>.</p>
+    </article>
+  </section>
+  <section>
+    <article>
+      <figure><img src="#" alt="Preview van wat je in de talentroute kan verwachten"/></figure>
+      <h1>Talent Route</h1>
+      <p>Trek op pad door de stad en ontdek de talenten van nu en vroeger diede stad verwonderen</p>
+    </article>
+    <article>
+      <figure><img src="#" alt="Preview van wat je in de sporenroute kan verwachten"/></figure>
+      <h1>Sporen Route</h1>
+      <p>Ontdek de wondere sporen nagelaten door koningen, legendes en natuurlijk ook de treinen in de stad.</p>
+    </article>
+    <article>
+      <figure><img src="#" alt="Preview van wat je in de toekomstroute kan verwachten"/></figure>
+      <h1>Toekomst Route</h1>
+      <p>Leer de scheppers van de toekomst kennen: van keizers tot helden en visies van morgen!</p>
+    </article>
+  </section>
+  <section>
+    <article>
+      <h1>Al een keertje testen?</h1>
+      <p>Wil je het nu al een keertje proberen? Haal je smartphone camera alvast boven:</p>
+      <ul>
+        <li><span>1</span> surf naar www.arunext.eu op je smartphone</li>
+        <li><span>2</span> doorloop de stappen</li>
+        <li><span>3</span> en scan het vlak hiernaast</li>
+      </ul>
+    </article>
+    <figure><img src="#" alt="Marker (foto) om de AR eens te testen"/></figure>
+  </section>
+  <section>
+    <ul>
+      <li>Kortrijk - Stationsplein</li>
+      <li>Doornik - Station</li>
+      <li>Rijsel - Euralille</li>
+      <li>Valenciennes - Stadshal</li>
+    </ul>
+  </section>
+</main>
+<footer>
+  <address>
+    <h1>NEXT Arts Festival</h1>
+    <p>Kapucijnenstraat 10</p>
+    <p>8500 Kortrijk</p>
+    <p>+32 56 22 10 01</p>
+    <p>info@nextfestival.eu</p>
+  </address>
+  <article>
+    <figure><img src="#" alt="Sponsor - Vlaanderen verbeelding werkt"/></figure>
+    <figure><img src="#" alt="Sponsor - Féderation Wallonie-Bruxelles"/></figure>
+    <figure><img src="#" alt="Sponsor - Région Hauts-de-France"/></figure>
+    <figure><img src="#" alt="Sponsor - Europa"/></figure>
+    <figure><img src="#" alt="Sponsor - Eurometropool"/></figure>
+    <figure><img src="#" alt="Sponsor - Nationale Lotterij"/></figure>
+    <figure><img src="#" alt="Sponsor - Stad Kortrijk"/></figure>
+    <figure><img src="#" alt="Sponsor - Valenciennes Metropole"/></figure>
+    <figure><img src="#" alt="Sponsor - Métropole Européene de Lille"/></figure>
+  </article>
+  <ul>
+    <li><a href="#">algemene voorwaarden</a></li>
+    <li><a href="#">privacyverklaring</a></li>
+    <li><a href="#">cookiebeleid</a></li>
+  </ul>
+  <a href="#">&copy; 2020</a>
+  <aside>
+    <img src="#" alt="Devine Logo"/>
+  </aside>
+</footer>
