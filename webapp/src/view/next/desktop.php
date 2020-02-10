45837f7520d11419a6cbad993c0cdad2676134d2
Lennon Puype <lennonpuype@hotmail.com>
2020-02-10 22:42:18 +0100

    Landingpagina semantiek & CSS grid opdeling

--- webapp/src/view/next/desktop.php
+++ webapp/src/view/next/desktop.php
@@ -1,16 +1,19 @@
-<header>
-  <nav>
+<header class="header">
+  <h1 class="hidden">Header</h1>
+  <nav class="header_languages">
+  <h2 class="hidden">Kies taal</h2>
     <ul>
       <li>NL</li>
       <li>EN</li>
       <li>FR</li>
     </ul>
   </nav>
-  <article>
+  <article class="header_title">
     <h1>a.r.u.next?</h1>
     <h2>the coolest city tours worldwide</h2>
   </article>
-  <address>
+  <address class="header_socials">
+  <h2 class="hidden">Social Media kanalen</h2>
     <ul>
       <li>facebook</li>
       <li>instagram</li>
@@ -19,34 +22,34 @@
   </address>
 </header>
 <main>
-  <section>
-    <figure><img src="#" alt="De installatie in het openbaar"/></figure>
-    <article>
+  <section class="intro">
+    <figure class="intro_image"><img src="#" alt="De installatie in het openbaar"/></figure>
+    <article class="intro_text">
       <h1>a.r.u.next ? de installatie</h1>
       <p>De a.r.u.next routes laten je op interactieve wijze kennis maken met de <b>talenten, sporen en toekomst</b> van de steden.</p>
       <h2>Waar kan je hem vinden?</h2>
       <p>De a.r.u.next installaties kan je vinden op centrale punten in de steden van de Eurometropool: Kortrijk, Lille, Tournai en Valenciennes. Weten waar? Check ze <span>hier</span>.</p>
     </article>
   </section>
-  <section>
-    <article>
+  <section class="routes_section">
+    <article class="route_preview">
       <figure><img src="#" alt="Preview van wat je in de talentroute kan verwachten"/></figure>
       <h1>Talent Route</h1>
       <p>Trek op pad door de stad en ontdek de talenten van nu en vroeger diede stad verwonderen</p>
     </article>
-    <article>
+    <article class="route">
       <figure><img src="#" alt="Preview van wat je in de sporenroute kan verwachten"/></figure>
       <h1>Sporen Route</h1>
       <p>Ontdek de wondere sporen nagelaten door koningen, legendes en natuurlijk ook de treinen in de stad.</p>
     </article>
-    <article>
+    <article class="route">
       <figure><img src="#" alt="Preview van wat je in de toekomstroute kan verwachten"/></figure>
       <h1>Toekomst Route</h1>
       <p>Leer de scheppers van de toekomst kennen: van keizers tot helden en visies van morgen!</p>
     </article>
   </section>
-  <section>
-    <article>
+  <section class="test">
+    <article class="test_text">
       <h1>Al een keertje testen?</h1>
       <p>Wil je het nu al een keertje proberen? Haal je smartphone camera alvast boven:</p>
       <ul>
@@ -55,43 +58,43 @@
         <li><span>3</span> en scan het vlak hiernaast</li>
       </ul>
     </article>
-    <figure><img src="#" alt="Marker (foto) om de AR eens te testen"/></figure>
+    <figure class="test_image"><img src="#" alt="Marker (foto) om de AR eens te testen"/></figure>
   </section>
   <section>
-    <ul>
-      <li>Kortrijk - Stationsplein</li>
-      <li>Doornik - Station</li>
-      <li>Rijsel - Euralille</li>
-      <li>Valenciennes - Stadshal</li>
+    <ul class="city_list">
+      <li class="city_list_item">Kortrijk - Stationsplein</li>
+      <li class="city_list_item">Doornik - Station</li>
+      <li class="city_list_item">Rijsel - Euralille</li>
+      <li class="city_list_item">Valenciennes - Stadshal</li>
     </ul>
   </section>
 </main>
-<footer>
-  <address>
+<footer class="footer">
+  <address class="footer_details">
     <h1>NEXT Arts Festival</h1>
     <p>Kapucijnenstraat 10</p>
     <p>8500 Kortrijk</p>
     <p>+32 56 22 10 01</p>
     <p>info@nextfestival.eu</p>
   </address>
-  <article>
-    <figure><img src="#" alt="Sponsor - Vlaanderen verbeelding werkt"/></figure>
-    <figure><img src="#" alt="Sponsor - Féderation Wallonie-Bruxelles"/></figure>
-    <figure><img src="#" alt="Sponsor - Région Hauts-de-France"/></figure>
-    <figure><img src="#" alt="Sponsor - Europa"/></figure>
-    <figure><img src="#" alt="Sponsor - Eurometropool"/></figure>
-    <figure><img src="#" alt="Sponsor - Nationale Lotterij"/></figure>
-    <figure><img src="#" alt="Sponsor - Stad Kortrijk"/></figure>
-    <figure><img src="#" alt="Sponsor - Valenciennes Metropole"/></figure>
-    <figure><img src="#" alt="Sponsor - Métropole Européene de Lille"/></figure>
+  <article class="footer_sponsors">
+    <figure><img src="./assets/img/landingpagina/sponsors/vlaanderen.png" width="200" alt="Sponsor - Vlaanderen verbeelding werkt"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/federation.png" width="200" alt="Sponsor - Féderation Wallonie-Bruxelles"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/hauts.png" width="200" alt="Sponsor - Région Hauts-de-France"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/europe.png" width="200" alt="Sponsor - Europa"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/em.png" width="200" alt="Sponsor - Eurometropool"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/nl.png" width="200" alt="Sponsor - Nationale Lotterij"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/kortrijk.png" width="200" alt="Sponsor - Stad Kortrijk"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/valenciennes.png" width="200" alt="Sponsor - Valenciennes Metropole"/></figure>
+    <figure><img src="./assets/img/landingpagina/sponsors/mel.png" width="200" alt="Sponsor - Métropole Européene de Lille"/></figure>
   </article>
-  <ul>
+  <ul class="footer_tags">
     <li><a href="#">algemene voorwaarden</a></li>
     <li><a href="#">privacyverklaring</a></li>
     <li><a href="#">cookiebeleid</a></li>
+    <a href="#">&copy; 2020</a>
   </ul>
-  <a href="#">&copy; 2020</a>
-  <aside>
-    <img src="#" alt="Devine Logo"/>
+  <aside class="devine_logo">
+    <img src="./assets/img/landingpagina/sponsors/devine.png" width="200" alt="Devine Logo"/>
   </aside>
 </footer>
