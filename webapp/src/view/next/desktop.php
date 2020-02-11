1456006409c650c4b828667ee5063e31943d8cd7
Lennon Puype <lennonpuype@hotmail.com>
2020-02-11 11:56:25 +0100

    Landingpagina CSS Update

--- webapp/src/view/next/desktop.php
+++ webapp/src/view/next/desktop.php
@@ -1,4 +1,4 @@
-<header class="header">
+<header class="landing_header">
   <h1 class="hidden">Header</h1>
   <nav class="header_languages">
   <h2 class="hidden">Kies taal</h2>
@@ -14,7 +14,7 @@
   </article>
   <address class="header_socials">
   <h2 class="hidden">Social Media kanalen</h2>
-    <ul>
+    <ul class="header_socials_list">
       <li><img src="./assets/img/landingpagina/socials/facebook.png" width="150" alt="#" class="header_socials_image"/></li>
       <li><img src="./assets/img/landingpagina/socials/instagram.png" width="150" alt="#" class="header_socials_image"/></li>
       <li><img src="./assets/img/landingpagina/socials/youtube.png" width="150" alt="#" class="header_socials_image"/></li>
@@ -26,39 +26,39 @@
     <figure class="intro_image"><img src="./assets/img/landingpagina/installation.png" width="300" alt="De installatie in het openbaar" class="intro_image_box"/></figure>
     <article class="intro_text">
       <h1 class="intro_text_title intro_text_title_big">a.r.u.next ? de installatie</h1>
-      <p class="intro_text_paragraph">De a.r.u.next routes laten je op interactieve wijze kennis maken met de <b class="intro_text_highlight">talenten, sporen en toekomst</b> van de steden.</p>
+      <p class="intro_text_paragraph intro_text_paragraph1">De a.r.u.next routes laten je op interactieve wijze kennis maken met de <b class="intro_text_highlight">talenten, sporen en toekomst</b> van de steden.</p>
       <h2 class="intro_text_title intro_text_title_small">Waar kan je hem vinden?</h2>
-      <p class="intro_text_paragraph">De a.r.u.next installaties kan je vinden op centrale punten in de steden van de Eurometropool: Kortrijk, Lille, Tournai en Valenciennes. Weten waar? Check ze <span class="intro_text_url intro_text_highlight">hier</span>.</p>
+      <p class="intro_text_paragraph intro_text_paragraph2">De a.r.u.next installaties kan je vinden op centrale punten in de steden van de Eurometropool: Kortrijk, Lille, Tournai en Valenciennes. Weten waar? Check ze <span class="intro_text_url intro_text_highlight">hier</span>.</p>
     </article>
   </section>
   <section class="routes_section">
     <article class="route_preview talent_route">
-      <figure><img src="./assets/img/landingpagina/routes/talent.png" width="200" alt="Preview van wat je in de talentroute kan verwachten"/></figure>
-      <h1>Talent Route</h1>
-      <p>Trek op pad door de stad en ontdek de talenten van nu en vroeger diede stad verwonderen</p>
+      <figure class="route_image"><img src="./assets/img/landingpagina/routes/talent.png" width="200" alt="Preview van wat je in de talentroute kan verwachten"/></figure>
+      <h1 class="route_title yellow">Talent Route</h1>
+      <p class="route_description">Trek op pad door de stad en ontdek de talenten van nu en vroeger diede stad verwonderen</p>
     </article>
     <article class="route_preview sporen_route">
-      <figure><img src="./assets/img/landingpagina/routes/sporen.png" width="200" alt="Preview van wat je in de sporenroute kan verwachten"/></figure>
-      <h1>Sporen Route</h1>
-      <p>Ontdek de wondere sporen nagelaten door koningen, legendes en natuurlijk ook de treinen in de stad.</p>
+      <figure class="route_image"><img src="./assets/img/landingpagina/routes/sporen.png" width="200" alt="Preview van wat je in de sporenroute kan verwachten"/></figure>
+      <h1 class="route_title green">Sporen Route</h1>
+      <p class="route_description">Ontdek de wondere sporen nagelaten door koningen, legendes en natuurlijk ook de treinen in de stad.</p>
     </article>
     <article class="route_preview toekomst_route">
-      <figure><img src="./assets/img/landingpagina/routes/toekomst.png" width="200" alt="Preview van wat je in de toekomstroute kan verwachten"/></figure>
-      <h1>Toekomst Route</h1>
-      <p>Leer de scheppers van de toekomst kennen: van keizers tot helden en visies van morgen!</p>
+      <figure class="route_image"><img src="./assets/img/landingpagina/routes/toekomst.png" width="200" alt="Preview van wat je in de toekomstroute kan verwachten"/></figure>
+      <h1 class="route_title red">Toekomst Route</h1>
+      <p class="route_description">Leer de scheppers van de toekomst kennen: van keizers tot helden en visies van morgen!</p>
     </article>
   </section>
   <section class="test">
     <article class="test_text">
       <h1>Al een keertje testen?</h1>
-      <p>Wil je het nu al een keertje proberen? Haal je smartphone camera alvast boven:</p>
+      <p class="test_text_paragraph">Wil je het nu al een keertje proberen? Haal je smartphone camera alvast boven:</p>
       <ul>
-        <li><span>1</span> surf naar www.arunext.eu op je smartphone</li>
-        <li><span>2</span> doorloop de stappen</li>
-        <li><span>3</span> en scan het vlak hiernaast</li>
+        <li class="test_text_list_item"><span class="test_text_bullet yellow_bg">1</span> surf naar www.arunext.eu op je smartphone</li>
+        <li class="test_text_list_item"><span class="test_text_bullet green_bg">2</span> doorloop de stappen</li>
+        <li class="test_text_list_item"><span class="test_text_bullet red_bg">3</span> en scan het vlak hiernaast</li>
       </ul>
     </article>
-    <figure class="test_image"><img src="#" alt="Marker (foto) om de AR eens te testen"/></figure>
+    <figure class="test_image"><img src="./assets/img/landingpagina/test.png"width="500" alt="Marker (foto) om de AR eens te testen"/></figure>
   </section>
   <section>
     <ul class="city_list">
