d984bad411510ef6ee2e88572fc4ad8124b72e5c
Nina Reunes <nina.reunes@student.howest.be>
2020-02-11 11:54:29 +0100

    carousel fixed

--- webapp/src/view/next/routes.php
+++ webapp/src/view/next/routes.php
@@ -106,8 +106,8 @@ if(!empty($_GET['l']) && empty($_SESSION['user'])){
           }
         ?>
       </article>
-      <section class="routes carousel" data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true }'></section>
-    </main>
+      <section class="routes carousel" data-flickity='{ "contain": true, "wrapAround": true, "prevNextButtons": false, "pageDots": false, "lazyload": 100 }'></section>
+     </main>
   <?php } ?>
 
   <?php if($_SESSION['user']['l'] == 'fr'){ ?>
