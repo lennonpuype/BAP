85a8512c2233a02db9d91813eed3468eb80e8dca
Lennon Puype <lennonpuype@hotmail.com>
2020-02-12 08:10:53 +0100

    Animate on scroll integrated

--- webapp/src/view/next/desktop.php
+++ webapp/src/view/next/desktop.php
@@ -1,6 +1,6 @@
 <header class="landing_header landingPage">
   <h1 class="hidden">Header</h1>
-  <nav class="header_languages">
+  <nav class="header_languages" data-aos="fade-down">
   <h2 class="hidden">Kies taal</h2>
     <ul class="header_languages_list">
       <li class="header_languages_list_item header_languages_list_item_active">NL</li>
@@ -8,7 +8,7 @@
       <li class="header_languages_list_item">FR</li>
     </ul>
   </nav>
-  <article class="header_title">
+  <article class="header_title" data-aos="fade-right">
     <h1 class="header_title_big">a.r.u.next?</h1>
     <h2 class="header_title_small">the coolest city tours worldwide</h2>
   </article>
@@ -23,8 +23,8 @@
 </header>
 <main>
   <section class="intro">
-    <figure class="intro_image"><img src="./assets/img/landingpagina/installation.png" width="300" alt="De installatie in het openbaar" class="intro_image_box"/></figure>
-    <article class="intro_text">
+    <figure class="intro_image" data-aos="fade-right"><img src="./assets/img/landingpagina/installation.png" width="300" alt="De installatie in het openbaar" class="intro_image_box"/></figure>
+    <article class="intro_text"  data-aos="fade-left">
       <h1 class="intro_text_title intro_text_title_big">a.r.u.next ? de installatie</h1>
       <p class="intro_text_paragraph intro_text_paragraph1">De a.r.u.next routes laten je op interactieve wijze kennis maken met de <b class="intro_text_highlight">talenten, sporen en toekomst</b> van de steden.</p>
       <h2 class="intro_text_title intro_text_title_small">Waar kan je hem vinden?</h2>
@@ -32,24 +32,24 @@
     </article>
   </section>
   <section class="routes_section">
-    <article class="route_preview talent_route">
+    <article class="route_preview talent_route" data-aos="fade-up">
       <figure class="route_image"><img src="./assets/img/landingpagina/routes/talent.png" width="200" alt="Preview van wat je in de talentroute kan verwachten"/></figure>
       <h1 class="route_title yellow">Talent Route</h1>
       <p class="route_description">Trek op pad door de stad en ontdek de talenten van nu en vroeger diede stad verwonderen</p>
     </article>
-    <article class="route_preview sporen_route">
+    <article class="route_preview sporen_route" data-aos="fade-down">
       <figure class="route_image"><img src="./assets/img/landingpagina/routes/sporen.png" width="200" alt="Preview van wat je in de sporenroute kan verwachten"/></figure>
       <h1 class="route_title green">Sporen Route</h1>
       <p class="route_description">Ontdek de wondere sporen nagelaten door koningen, legendes en natuurlijk ook de treinen in de stad.</p>
     </article>
-    <article class="route_preview toekomst_route">
+    <article class="route_preview toekomst_route" data-aos="fade-up">
       <figure class="route_image"><img src="./assets/img/landingpagina/routes/toekomst.png" width="200" alt="Preview van wat je in de toekomstroute kan verwachten"/></figure>
       <h1 class="route_title red">Toekomst Route</h1>
       <p class="route_description">Leer de scheppers van de toekomst kennen: van keizers tot helden en visies van morgen!</p>
     </article>
   </section>
   <section class="test">
-    <article class="test_text">
+    <article class="test_text" data-aos="fade-right">
       <h1 class="test_text_title">Al een keertje testen?</h1>
       <p class="test_text_paragraph">Wil je het nu al een keertje proberen? Haal je smartphone camera alvast boven:</p>
       <ul class="test_text_list">
@@ -58,11 +58,11 @@
         <li class="test_text_list_item"><span class="test_text_bullet red_bg">3</span> en scan het vlak hiernaast</li>
       </ul>
     </article>
-    <figure class="test_image"><img src="./assets/img/landingpagina/test.png" width="500" class="test_image_self" alt="Marker (foto) om de AR eens te testen"/></figure>
-    <figure class="test_image_responsive"><img src="./assets/img/landingpagina/test_responsive.png" width="500" class="test_image_self" alt="Marker (foto) om de AR eens te testen"/></figure>
+    <figure class="test_image" data-aos="fade-left"><img src="./assets/img/landingpagina/test.png" width="500" class="test_image_self" alt="Marker (foto) om de AR eens te testen"/></figure>
+    <figure class="test_image_responsive" data-aos="fade-up"><img src="./assets/img/landingpagina/test_responsive.png" width="500" class="test_image_self" alt="Marker (foto) om de AR eens te testen"/></figure>
   </section>
   <section>
-    <ul class="city_list" id="places">
+    <ul class="city_list" id="places" data-aos="fade-up">
       <li class="city_list_item kortrijk">Kortrijk - Stationsplein</li>
       <li class="city_list_item tournai">Doornik - Station</li>
       <li class="city_list_item lille">Rijsel - Euralille</li>
@@ -99,3 +99,6 @@
     <img src="./assets/img/landingpagina/sponsors/devine.png" width="200" alt="Devine Logo"/>
   </aside>
 </footer>
+<script>
+  AOS.init();
+</script>
