a77eda9c99aecef38d61c34ccc84ad2a92fa6d7c
Lennon Puype <lennonpuype@hotmail.com>
2020-02-12 21:29:22 +0100

    Accordion toegevoegd

--- webapp/src/js/script.js
+++ webapp/src/js/script.js
@@ -29,6 +29,20 @@
       }
     });
 
+    const acc = document.getElementsByClassName("accordion");
+
+    for (let i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        //this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+
     // const $allNavItems = document.querySelectorAll(`.nav-link`);
     // const navItemArray = Array.from($allNavItems);
     // navItemArray.map(item => {
