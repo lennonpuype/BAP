2206a14187fc25460e52becf7e6652b8fcf94b32
Nina Reunes <nina.reunes@student.howest.be>
2020-02-12 19:03:24 +0100

    merge

--- webapp/src/controller/NextController.php
+++ webapp/src/controller/NextController.php
@@ -172,6 +172,7 @@ class NextController extends Controller {
     }
 
     array_push($_SESSION['user']['activeRoutes'], $_COOKIE['activeRoute']);
+    var_dump($_COOKIE);
     $this->set('visitedPoints', $_SESSION['user']['activeRoutes']);
   }
 
