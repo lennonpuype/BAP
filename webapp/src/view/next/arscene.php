bac4bc3bb729ff328d409f81f6f926b8a156b11d
Lennon Puype <lennonpuype@hotmail.com>
2020-02-14 08:14:46 +0100

    Gifkes toegevoegd voor alle steden

--- webapp/src/view/next/arscene.php
+++ webapp/src/view/next/arscene.php
@@ -14,38 +14,40 @@
   </a-asset-item>
   <a-asset-item id="gltf7" src="./assets/ar/gltf/kalle-manten.glb">
   </a-asset-item>
+  <a-asset-item id="next_gltf" src="./assets/ar/gltf/next-logo.glb">
+  </a-asset-item>
 </a-assets>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-sticker.patt' name="0" class="marker 0">
-  <a-entity gltf-model="#gltf7" light="type: ambient; color: #BBB" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" light="type: ambient; color: #BBB" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-1.patt' name="1" class="marker 1">
-  <a-entity gltf-model="#gltf7" light="color: #AFA; intensity: .2" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" light="color: #AFA; intensity: .2" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-2.patt' name="2" class="marker 2">
-  <a-entity gltf-model="#gltf7" light="type: ambient; color: rgba(255,255,255,.3)" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" light="type: ambient; color: rgba(255,255,255,.3)" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-3.patt' name="3" class="marker 3">
-  <a-entity gltf-model="#gltf7" light="type: ambient; color: #FFF" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" light="type: ambient; color: #FFF" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-4.patt' name="4" class="marker 4">
-  <a-entity gltf-model="#gltf7" light="color: #FFF; intensity: 1.5" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" light="color: #FFF; intensity: 1.5" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-5.patt' name="5" class="marker 5">
-  <a-entity gltf-model="#gltf7" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-6.patt' name="6" class="marker 6">
-  <a-entity gltf-model="#gltf7" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-marker type='pattern' url='./assets/ar/data/pattern-next.patt' name="next" class="marker next">
-  <a-entity gltf-model="#gltf7" scale=".02 .02 .02" animation-mixer></a-entity>
+  <a-entity gltf-model="#next_gltf" scale=".02 .02 .02" animation-mixer></a-entity>
 </a-marker>
 
 <a-entity camera>
